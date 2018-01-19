a7a4f555fea068a4cc245a4f1c1d719c42454022
Ashutosh <ashutoshsinghtomar.ashu@gmail.com>
2018-01-19 12:34:33 +0530

    Getting Current Latitude and Longitude

--- app/src/main/java/com/example/ashu/weatherforecast/MainActivity.java
+++ app/src/main/java/com/example/ashu/weatherforecast/MainActivity.java
@@ -1,13 +1,127 @@
 package com.example.ashu.weatherforecast;
 
+import android.*;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+
+     TextView currentCity;
+    GoogleApiClient mGoogleApiClient;
+    LocationRequest mLocationRequest;
+    final int RequestCode=10;
+    final String logTag="Weather Forecast";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        currentCity = findViewById(R.id.currentCity);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case RequestCode:
+                getLocation();
+                break;
+        }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+        mLocationRequest = LocationRequest.create();
+
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(1000);
+        getLocation();
+
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(logTag,"Connction Suspended");
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.i(logTag,"Connction Failed");
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.i(logTag,""+location);
+
+        String currentLocation=""+location.getLatitude()+"    "+location.getLongitude();
+        currentCity.setText(currentLocation);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mGoogleApiClient.disconnect();
+    }
+
+
+    public void getLocation(){
+
+
+
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+
+
+                if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
+                    requestPermissions(new String[]{
+                        android.Manifest.permission.INTERNET,
+                        android.Manifest.permission.ACCESS_FINE_LOCATION,
+                        android.Manifest.permission.ACCESS_COARSE_LOCATION
+                },RequestCode);
+
+            }
+
+        }else
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, MainActivity.this);
     }
 }
